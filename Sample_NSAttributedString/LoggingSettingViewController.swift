bcd7e43c998b7544b4f8227998ef692b0fbc31da
Kosuke Tomita <s163404@gmail.com>
2021-10-21 00:09:51 +0900

    ボタンで画面遷移

---
+++ Sample_NSAttributedString/LoggingSettingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoggingSettingViewController.swift
+//  Sample_NSAttributedString
+//
+//  Created by 冨田康介 on 2021/10/20.
+//
+
+import UIKit
+
+class LoggingSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Sample_NSAttributedString/ViewController.swift
+++ Sample_NSAttributedString/ViewController.swift
@@ -10,18 +10,31 @@ import UIKit
 class ViewController: UIViewController {
 //    var textView = UITextView()
     var textView = CustomTextView()
+    var button = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        button.setTitle("設定画面へ", for: .normal)
+        button.backgroundColor = .link
+        button.titleEdgeInsets = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        button.addTarget(self, action: #selector(tapButton), for: .touchUpInside)
+        
         view.addSubview(textView)
+        view.addSubview(button)
         
         textView.translatesAutoresizingMaskIntoConstraints = false
+        button.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             textView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             textView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             textView.widthAnchor.constraint(equalTo: view.widthAnchor),
-            textView.heightAnchor.constraint(equalToConstant: 32)
+            textView.heightAnchor.constraint(equalToConstant: 32),
+            
+            button.topAnchor.constraint(equalTo: view.centerYAnchor, constant: 100),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            button.widthAnchor.constraint(equalTo: view.widthAnchor),
+//            button.heightAnchor.constraint(equalToConstant: 44),
         ])
         
         let font = UIFont(name: "HiraKakuProN-W3", size: 22)
@@ -37,6 +50,12 @@ class ViewController: UIViewController {
                                                     NSAttributedString.Key.paragraphStyle: style
                                                    ])
         textView.attributedText = attrString
+    }
+    
+    @objc func tapButton() {
+        let vc = LoggingSettingViewController()
+        let nc = UINavigationController(rootViewController: vc)
+        self.present(nc, animated: true, completion: nil)
     }
 
 
